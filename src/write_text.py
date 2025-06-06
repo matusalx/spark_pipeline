fd8536d3f074b70fb06765c25dd25b08c06f46bd
Vano Gurgenishvili <>
2025-06-06 23:07:51 +0400

    add  update readme, docs, col partition , df.repartition

--- src/write_text.py
+++ src/write_text.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from delta.tables import DeltaTable
 from delta import configure_spark_with_delta_pip
 from PIL import Image
-from pyspark.sql.functions import sha2, concat_ws, col, trim
+from pyspark.sql.functions import sha2, concat_ws, col, trim, regexp_replace, col, lower
 from pyspark.sql.types import StructType, StructField, StringType, BinaryType
 from pyspark.sql.types import StructType
 import os
@@ -20,7 +20,8 @@ spark = SparkSession.builder \
         .getOrCreate()
 
 
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+parent_dir= "/Users/vano/Desktop/spark_homowork"
 delta_lake_dir = os.path.join (parent_dir, "tmp/delta_text")
 
 text_dir = os.path.join (parent_dir, "data_sample/Texts/Fake.csv")
@@ -55,13 +56,19 @@ merge_condition = "target.row_hash = source.row_hash"
 
 # if not exists then crate or else insert/update
 # use partition while creating the table
+# use hash to reduce date text lenght (long text cannot be used as a partition)
+deduped_df = deduped_df.withColumn("date", sha2(col("date"), 256))
 
-# TODO add partition
+# after repartiion there is no warnong for heap space
+deduped_df = deduped_df.repartition(200)
+
+
 if not DeltaTable.isDeltaTable(spark, delta_lake_dir):
     try:
         deduped_df.write \
             .format("delta") \
             .mode("overwrite") \
+            .partitionBy("date") \
             .save(delta_lake_dir)
         print("Delta table created. with name: ", delta_lake_dir)
     except Exception as e:
